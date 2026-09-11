364975020d0c018ecdcbe929f195f75036835e93
ANJANI  BEESU <anjanibeesu@gmail.com>
2026-09-11 20:00:28 +0530

    feat: add RankRoute landing page

---
+++ app/page.tsx
@@ -0,0 +1,1 @@
+export default function Home() { return <main className="min-h-screen p-10"><h1 className="text-6xl font-black">Your rank. <span className="text-[#f4c542]">Your chances.</span></h1></main>; }
